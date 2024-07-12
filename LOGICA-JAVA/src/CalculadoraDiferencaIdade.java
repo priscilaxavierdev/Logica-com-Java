1b3145500f52fa899b702930c72429e3a7500a7d
Priscila Xavier <priscilaxavier672@gmail.com>
2024-07-12 18:40:27 -0300

    Delete LOGICA-JAVA directory

--- LOGICA-JAVA/src/CalculadoraDiferencaIdade.java
+++
@@ -1,9 +0,0 @@
-public class CalculadoraDiferencaIdade {
-    public static void main(String[] args){
-        int firstAge = 30;
-        int secondAge = 20;
-        int ageDifference = firstAge - secondAge;
-        System.out.println("A difernca entre as idades eh: " + ageDifference);
-    }
-}
-
