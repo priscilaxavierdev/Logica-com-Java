b24360d0d71889c6d6d40937963e7f7d096e1017
Priscila Xavier <priscilaxavier672@gmail.com>
2024-07-13 15:45:49 -0300

    Add files via upload

---
+++ LOGICA-JAVA/src/SistemSolar.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class SistemSolar {
+    public static void main(String[] args) {
+        String planeta = "Plutão";
+            System.out.println(planeta);
+    }
+}
