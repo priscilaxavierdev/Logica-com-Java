12913d483a50d98322f8608b4d7a259e404ade85
Priscila Xavier <priscilaxavier672@gmail.com>
2024-07-16 20:14:31 -0300

    Delete LOGICA-JAVA directory

--- LOGICA-JAVA/src/ConcatenarElementos.java
+++
@@ -1,7 +0,0 @@
-public class ConcatenarElementos {
-    public static void main(String[] args){
-    String nome = "Maria";
-    int idade = 31;
-    double renda = 4000.0D;
-    System.out.printf("%s tem %d anos e ganha %.2f reais%n", nome, idade, renda);}
-}
--- LOGICA-JAVA/src/SistemSolar.java
+++
@@ -1,8 +0,0 @@
-import org.w3c.dom.ls.LSOutput;
-
-public class SistemSolar {
-    public static void main(String[] args) {
-        String planeta = "Plutão";
-            System.out.println(planeta);
-    }
-}
